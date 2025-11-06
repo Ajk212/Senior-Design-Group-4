1cfa3d2bb26eee1de7e7141765e722965af6ddbf
Aaron <Aaronk828@gmail.com>
2025-11-05 20:08:00 -0500

    esp32 main file updates

--- ESP32-Model-Code/main.cpp
+++ ESP32-Model-Code/main.cpp
@@ -7,13 +7,99 @@
 
 constexpr int kTensorArenaSize = 10 * 1024;
 uint8_t tensor_arena[kTensorArenaSize];
+int WINDOW_SIZE = 20;
 
 tfile::MicroInterpreter* interpreter = nullptr;
 TfLiteTensor* input = nullptr;
 TfLiteTensor* output = nullptr;
 
-void setup(){
-    Serial.begin(115200);
+const char* gesture_names[] = {
+    "rest" ,
+    "tilt_left",
+    "tilt_right",
+    "tilt_forward",
+    "tilt_backward",
+};
+
+extern "C" void app_main() {
     delay(2000);
-    
+     
+
+    const tflite::Model* model = tflite::GetModel(Lite_LSTM_Test_tflite);
+
+    if (model->version() != TFLITE_SCHEMA_VERSION) {
+        printf("Model schema version mismatch!\n");
+        while(1);
+    }
+
+    static tflite::AllOpsResolver resolver;
+
+    static tflite::MicroInterpreter static_interpreter(
+        model, resolver, tensor_arena, kTensorArenaSize);
+    interpreter = &static_interpreter;
+
+    if (interpreter->AllocateTensors() != kTfLiteOk) {
+        printf("Tensor allocation failed!\n");
+        while(1);
+    }
+
+    input = interpreter->input(0);
+    output = interpreter->output(0);
+
+    printf("Model loaded successfully!\n");
+
+    //Main model inference loop
+    while (1) {
+
+        //Read senor data frame - 20 values of 3-axis gyro
+        for (int t = 0; t < WINDOW_SIZE; t++) {
+            float gx, gy, gz;
+            if (mpu6050_read_gyroscope_sensor(MPU6050_I2C_ADDR_1, &gx, &gy, &gz) != ESP_OK) {
+                printf("Failed to read IMU1 gyro at step %d\n", t);
+            }
+
+            input->data.f[t * 3 + 0] = gx;
+            input->data.f[t * 3 + 1] = gy;
+            input->data.f[t * 3 + 2] = gz;
+
+            vTaskDelay(pdMS_TO_TICKS(10)); 
+        }
+
+        //Run model - If failed print and continue
+        if (interpreter->Invoke() != kTfLiteOk) {
+            printf("Model inference failed!\n");
+            continue;
+        }
+
+        //Locate most probable gesture
+        int max_index = 0;
+        float max_value = output->data.f[0];
+        for (int i = 1; i < output->dims->data[1]; i++) {
+            if (output->data.f[i] > max_value) {
+                max_value = output->data.f[i];
+                max_index = i;
+            }
+        }
+
+        //Take index and convert to gesture name
+        const char* predicted_gesture = gesture_names[max_index];
+
+        //Send result to buffer
+        char ble_buffer[32]; 
+        memset(ble_buffer, 0, sizeof(ble_buffer));
+        strncpy(ble_buffer, predicted_gesture, sizeof(ble_buffer) - 1);
+
+        //Send to computer via BLE
+        if (notifications_enabled && current_conn_id != 0xFFFF) {
+            esp_err_t err = esp_ble_gatts_send_indicate(
+                current_gatts_if,
+                current_conn_id,
+                gl_profile_tab[PROFILE_A_APP_ID].char_handle,
+                strlen(ble_buffer),
+                (uint8_t *)ble_buffer,
+                false 
+            );
+
+    }
+    }
 }
